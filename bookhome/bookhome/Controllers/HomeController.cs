4aa9a71f9f0c8e37cf098fc470dc4411dcc24d19
hoaitu <hoaitugl@gmail.com>
2020-07-21 23:46:46 +0700

    update config MonHoc, DangKy , LopHoc

---
+++ bookhome/bookhome/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bookhome.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Shop()
+        {
+            
+            return View();
+        }
+
+        public ActionResult Cart()
+        {
+            return View();
+        }
+        
+            public ActionResult Checkout()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult My_account()
+        {
+            return View();
+        }
+
+        public ActionResult Singup()
+        {
+            return View();
+        }
+
+        public ActionResult Single_product()
+        {
+            return View();
+        }
+
+
+        public ActionResult Wishlist()
+        {
+            return View();
+        }
+    }
+}
---
+++ bookhome/bookhome/DBUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace bookhome
+{
+    class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "192.168.205.130";
+            int port = 3306;
+            string database = "simplehr";
+            string username = "root";
+            string password = "1234";
+
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);
+        }
+
+    }
+}
